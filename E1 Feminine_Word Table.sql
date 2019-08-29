1d40a6fc7f744514ac2a6d2c232f4ab8dd769db2
Bob Burggraaf <Fams@churchofjesuschrist.org>
2019-08-29 11:20:45 -0600

    Added more words

--- E1 Feminine_Word Table.sql
+++ E1 Feminine_Word Table.sql
@@ -65,8 +65,10 @@ VALUES (
         , ( 
         10002 ----> Feminine_Word_Id
         , 'Nymphet' ----> Feminine_Word
-        , '' ----> Feminine_Word_Definition
-        , ''----> Feminine_Word_Key_Words
+        , 'An attractive and sexually mature young girl.
+            A sexually precocious girl barely in her teens.
+            ' ----> Feminine_Word_Definition
+        , 'Attractive, Teen, Sexually Mature'----> Feminine_Word_Key_Words
         , 'N' ----> Feminine_Word_Used_Yn
         , 1 ----> Feminine_Word_Rating
         , '2019-08-29' ----> Feminine_Word_Input_Date
@@ -74,9 +76,10 @@ VALUES (
         )
         , ( 
         10003 ----> Feminine_Word_Id
-        , '' ----> Feminine_Word
-        , '' ----> Feminine_Word_Definition
-        , ''----> Feminine_Word_Key_Words
+        , 'Philander' ----> Feminine_Word
+        , 'To talk or behave amorously without serious intentions.
+            Have amorous affairs.' ----> Feminine_Word_Definition
+        , 'Talk, Behavior, Amorous, Intentions'----> Feminine_Word_Key_Words
         , 'N' ----> Feminine_Word_Used_Yn
         , 1 ----> Feminine_Word_Rating
         , '2019-08-29' ----> Feminine_Word_Input_Date
@@ -84,9 +87,13 @@ VALUES (
         )
         , ( 
         10004 ----> Feminine_Word_Id
-        , '' ----> Feminine_Word
-        , '' ----> Feminine_Word_Definition
-        , ''----> Feminine_Word_Key_Words
+        , 'Hoyden' ----> Feminine_Word
+        , 'A girl who behaves in a boyish manner.
+            A boisterous girl.
+            A girl or woman of saucy, boisterous, or carefree behavior.
+            Adjuctive: Hoydenish
+            ' ----> Feminine_Word_Definition
+        , 'Girl, Boyish, Boisterous'----> Feminine_Word_Key_Words
         , 'N' ----> Feminine_Word_Used_Yn
         , 1 ----> Feminine_Word_Rating
         , '2019-08-29' ----> Feminine_Word_Input_Date
@@ -94,19 +101,23 @@ VALUES (
         )
         , ( 
         10005 ----> Feminine_Word_Id
-        , '' ----> Feminine_Word
-        , '' ----> Feminine_Word_Definition
-        , ''----> Feminine_Word_Key_Words
+        , 'Quean' ----> Feminine_Word
+        , 'An impudent or ill-behaved girl or woman.
+            A prostitute.
+            A woman that is young or unmarried.
+            ' ----> Feminine_Word_Definition
+        , 'Prostitute, Young, Unmarried'----> Feminine_Word_Key_Words
         , 'N' ----> Feminine_Word_Used_Yn
-        , 1 ----> Feminine_Word_Rating
+        , 2 ----> Feminine_Word_Rating
         , '2019-08-29' ----> Feminine_Word_Input_Date
         , GETDATE() ----> Feminine_Word_Update_Date
         )
         , ( 
         10006 ----> Feminine_Word_Id
-        , '' ----> Feminine_Word
-        , '' ----> Feminine_Word_Definition
-        , ''----> Feminine_Word_Key_Words
+        , 'Doxy' ----> Feminine_Word
+        , 'Mistress or Prostitue.
+            ' ----> Feminine_Word_Definition
+        , 'Mistress, Prostitue'----> Feminine_Word_Key_Words
         , 'N' ----> Feminine_Word_Used_Yn
         , 1 ----> Feminine_Word_Rating
         , '2019-08-29' ----> Feminine_Word_Input_Date
@@ -114,19 +125,23 @@ VALUES (
         )
         , ( 
         10007 ----> Feminine_Word_Id
-        , '' ----> Feminine_Word
-        , '' ----> Feminine_Word_Definition
-        , ''----> Feminine_Word_Key_Words
+        , 'Dominatrix' ----> Feminine_Word
+        , 'A dominating woman
+            (Especially one who plays that role in a sadomasochistic sexual relationship).
+            ' ----> Feminine_Word_Definition
+        , 'Dominating'----> Feminine_Word_Key_Words
         , 'N' ----> Feminine_Word_Used_Yn
-        , 1 ----> Feminine_Word_Rating
+        , 3 ----> Feminine_Word_Rating
         , '2019-08-29' ----> Feminine_Word_Input_Date
         , GETDATE() ----> Feminine_Word_Update_Date
         )
         , ( 
         10008 ----> Feminine_Word_Id
-        , '' ----> Feminine_Word
-        , '' ----> Feminine_Word_Definition
-        , ''----> Feminine_Word_Key_Words
+        , 'Sylph' ----> Feminine_Word
+        , 'An elemental being believed to inhabit the air.
+            A slender graceful young woman.
+            ' ----> Feminine_Word_Definition
+        , 'Slender, Graceful, Inhabit the air'----> Feminine_Word_Key_Words
         , 'N' ----> Feminine_Word_Used_Yn
         , 1 ----> Feminine_Word_Rating
         , '2019-08-29' ----> Feminine_Word_Input_Date
@@ -134,6 +149,54 @@ VALUES (
         )
         , ( 
         10009 ----> Feminine_Word_Id
+        , 'Slattern' ----> Feminine_Word
+        , 'A dirty untidy woman.
+            A prostitute who attracts customers by walking the streets.
+            ' ----> Feminine_Word_Definition
+        , 'Prostitute, Dirty'----> Feminine_Word_Key_Words
+        , 'N' ----> Feminine_Word_Used_Yn
+        , 1 ----> Feminine_Word_Rating
+        , '2019-08-29' ----> Feminine_Word_Input_Date
+        , GETDATE() ----> Feminine_Word_Update_Date
+        )
+        , ( 
+        10010 ----> Feminine_Word_Id
+        , 'Nullipara' ----> Feminine_Word
+        , '(Obstetrics) A woman who has never given birth to a child.
+            ' ----> Feminine_Word_Definition
+        , 'Birth, No Child'----> Feminine_Word_Key_Words
+        , 'N' ----> Feminine_Word_Used_Yn
+        , 1 ----> Feminine_Word_Rating
+        , '2019-08-29' ----> Feminine_Word_Input_Date
+        , GETDATE() ----> Feminine_Word_Update_Date
+        )
+        , ( 
+        10011 ----> Feminine_Word_Id
+        , 'Gravida' ----> Feminine_Word
+        , 'A pregnant woman
+            The number of the pregnancies that a woman is in
+            ' ----> Feminine_Word_Definition
+        , 'Pregant'----> Feminine_Word_Key_Words
+        , 'N' ----> Feminine_Word_Used_Yn
+        , 1 ----> Feminine_Word_Rating
+        , '2019-08-29' ----> Feminine_Word_Input_Date
+        , GETDATE() ----> Feminine_Word_Update_Date
+        )
+        , ( 
+        10012 ----> Feminine_Word_Id
+        , 'Motility' ----> Feminine_Word
+        , 'A change of position that does not entail a change of location.
+            The ability to move spontaneously and independently.
+            The ability to move without help.
+            ' ----> Feminine_Word_Definition
+        , 'Movement, Independent, Change of Position'----> Feminine_Word_Key_Words
+        , 'N' ----> Feminine_Word_Used_Yn
+        , 3 ----> Feminine_Word_Rating
+        , '2019-08-29' ----> Feminine_Word_Input_Date
+        , GETDATE() ----> Feminine_Word_Update_Date
+        )
+        , ( 
+        10013 ----> Feminine_Word_Id
         , '' ----> Feminine_Word
         , '' ----> Feminine_Word_Definition
         , ''----> Feminine_Word_Key_Words
@@ -143,7 +206,67 @@ VALUES (
         , GETDATE() ----> Feminine_Word_Update_Date
         )
         , ( 
-        10010 ----> Feminine_Word_Id
+        10014 ----> Feminine_Word_Id
+        , '' ----> Feminine_Word
+        , '' ----> Feminine_Word_Definition
+        , ''----> Feminine_Word_Key_Words
+        , 'N' ----> Feminine_Word_Used_Yn
+        , 1 ----> Feminine_Word_Rating
+        , '2019-08-29' ----> Feminine_Word_Input_Date
+        , GETDATE() ----> Feminine_Word_Update_Date
+        )
+        , ( 
+        10015 ----> Feminine_Word_Id
+        , '' ----> Feminine_Word
+        , '' ----> Feminine_Word_Definition
+        , ''----> Feminine_Word_Key_Words
+        , 'N' ----> Feminine_Word_Used_Yn
+        , 1 ----> Feminine_Word_Rating
+        , '2019-08-29' ----> Feminine_Word_Input_Date
+        , GETDATE() ----> Feminine_Word_Update_Date
+        )
+        , ( 
+        10016 ----> Feminine_Word_Id
+        , '' ----> Feminine_Word
+        , '' ----> Feminine_Word_Definition
+        , ''----> Feminine_Word_Key_Words
+        , 'N' ----> Feminine_Word_Used_Yn
+        , 1 ----> Feminine_Word_Rating
+        , '2019-08-29' ----> Feminine_Word_Input_Date
+        , GETDATE() ----> Feminine_Word_Update_Date
+        )
+        , ( 
+        10017 ----> Feminine_Word_Id
+        , '' ----> Feminine_Word
+        , '' ----> Feminine_Word_Definition
+        , ''----> Feminine_Word_Key_Words
+        , 'N' ----> Feminine_Word_Used_Yn
+        , 1 ----> Feminine_Word_Rating
+        , '2019-08-29' ----> Feminine_Word_Input_Date
+        , GETDATE() ----> Feminine_Word_Update_Date
+        )
+        , ( 
+        10018 ----> Feminine_Word_Id
+        , '' ----> Feminine_Word
+        , '' ----> Feminine_Word_Definition
+        , ''----> Feminine_Word_Key_Words
+        , 'N' ----> Feminine_Word_Used_Yn
+        , 1 ----> Feminine_Word_Rating
+        , '2019-08-29' ----> Feminine_Word_Input_Date
+        , GETDATE() ----> Feminine_Word_Update_Date
+        )
+        , ( 
+        10019 ----> Feminine_Word_Id
+        , '' ----> Feminine_Word
+        , '' ----> Feminine_Word_Definition
+        , ''----> Feminine_Word_Key_Words
+        , 'N' ----> Feminine_Word_Used_Yn
+        , 1 ----> Feminine_Word_Rating
+        , '2019-08-29' ----> Feminine_Word_Input_Date
+        , GETDATE() ----> Feminine_Word_Update_Date
+        )
+        , ( 
+        10020 ----> Feminine_Word_Id
         , '' ----> Feminine_Word
         , '' ----> Feminine_Word_Definition
         , ''----> Feminine_Word_Key_Words
